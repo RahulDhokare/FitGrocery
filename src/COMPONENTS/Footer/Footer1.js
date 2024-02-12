23c253bb626765808f6d3e734034351040d551f7
Rahul Dhokare <dhokarer24@gmail.com>
2024-02-12 10:08:55 +0530

    footer complete

---
+++ src/COMPONENTS/Footer/Footer1.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Footer1.css'
+import veges from '../../ASSETS/Images/veges.png'
+const Footer1 = () => {
+  return (
+    <div className='footer1'>
+      <div className='left'>
+        <img src={veges} alt='veges'/>
+      </div>
+      <div className='right'>
+        <h1>
+        Fresh Vegetavles & Fruits at your doorstep
+        </h1>
+        <p>We deliver fresh vegetables & fruits at your doorstep. We deliver fresh vegetables & fruits at your doorstep.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer1
--- src/PAGES/HomePage/Home.js
+++ src/PAGES/HomePage/Home.js
@@ -3,7 +3,8 @@ import Navbar from '../../COMPONENTS/Navbar/Navbar'
 import BannerSlider from '../../COMPONENTS/Banners/BannerSlider'
 import { HomeCategorys } from '../../COMPONENTS/Catagory/HomeCategorys'
 import Product_Slider from '../../COMPONENTS/Product/Product_Slider'
- 
+import Footer1 from '../../COMPONENTS/Footer/Footer1' 
+import Footer2 from '../../COMPONENTS/Footer/Footer2'
 function Home() {
   return (
     <div>
@@ -11,6 +12,8 @@ function Home() {
         <BannerSlider/>
         <HomeCategorys/>
         <Product_Slider/>
+        <Footer1/>
+        <Footer2/>
         Home</div>
   )
 }
